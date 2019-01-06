3d5538e8c7d57ec6e2d2fb28b1f32ca3c1be2514
Chandra Mishra <Prasadchandra18@gmail.com>
2019-01-06 11:05:12 +0530

    Update employeemodels.js

--- models/employeemodels.js
+++ models/employeemodels.js
@@ -1,13 +1,30 @@
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost:27017/Employee',{ useNewUrlParser: true });
 
 var EmployeeSchema = new Schema({
-    firstName : String,
-    lastName : String,
-    age : Number,
-    address: String
-});
+    employeeId : {
+        type : String,
+        required : true
+    },
+    firstName : {
+        type :String,
+        required : true
+    },
+    lastName :{
+        type : String,
+        required : false
+    }, 
+    age : {
+        type :Number,
+        required : true
+    }, //{ Type : Number, required : true}
+    address: {
+        type : String,
+        required : true
+    }
+}/*,{collection : 'employee-data'}*/);
 
 
 var Employee = mongoose.model('Employee',EmployeeSchema);
